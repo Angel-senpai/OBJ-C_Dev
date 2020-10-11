8184fac829defbd9e1273c51e4a58274e1ac5e4a
Angel-senpai <daniil.murygin68@gmail.com>
2020-10-11 07:06:44 +0300

    Создание приложений и основных UI компонентов без применения Interface Builder

---
+++ OBJ-C_Dev/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  OBJ-C_Dev
+//
+//  Created by Даниил Мурыгин on 10.10.2020.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ OBJ-C_Dev/SecondViewController.h
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.h
+//  OBJ-C_Dev
+//
+//  Created by Даниил Мурыгин on 10.10.2020.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SecondViewController : UIViewController
+@property (nonatomic,strong) UIButton *button;
+
+@end
+
+NS_ASSUME_NONNULL_END
